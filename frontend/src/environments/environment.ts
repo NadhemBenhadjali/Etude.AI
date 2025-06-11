a87506fc40bb938a10e95f0e07bd6708a9c7fa30
Mohamed Aziz Souid <mohamedaziz.souid@ensi-uma.tn>
2025-06-11 16:07:37 +0100

    TTS integrated succefully

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBase: 'https://0c63-34-29-229-240.ngrok-free.app',
+  apiBase2: 'http://127.0.0.1:8000'
+}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiBase: 'https://902b-104-198-108-63.ngrok-free.app'
-}
