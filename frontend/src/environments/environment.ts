1415b032cbf0b72a9d711f5092d907016d2ab455
Nadhem Benhadjali <nadhembenhadjali@gmail.com>
2025-06-12 15:55:35 +0100

    Final-Version

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBase: 'https://8ad8-34-56-159-153.ngrok-free.app',
+  apiBase: 'https://f91d-34-90-35-83.ngrok-free.app',
   apiBase2: 'http://127.0.0.1:8000'
 }
