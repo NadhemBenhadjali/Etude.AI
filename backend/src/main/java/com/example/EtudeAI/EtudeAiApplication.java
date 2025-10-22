940d4fdd08881bbc8810b252c7883d3db5e76642
mohamed ali ben ltaief <medaliboi@gmail.com>
2025-10-22 15:45:17 +0100

    initial commit

---
+++ backend/src/main/java/com/example/EtudeAI/EtudeAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EtudeAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtudeAiApplication.class, args);
+	}
+
+}
