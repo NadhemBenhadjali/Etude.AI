5cff025f85527d703f556411b5628ff25b9b6d56
Khalil Krifi <Khalil.Krifi@ensi-uma.tn>
2025-12-27 03:41:44 +0100

    add auth to swagger

--- backend/src/main/java/com/example/EtudeAI/config/OpenApiConfig.java
+++ backend/src/main/java/com/example/EtudeAI/config/OpenApiConfig.java
@@ -1,19 +1,32 @@
 package com.example.EtudeAI.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
             .info(new Info()
-                .title("My Spring Boot API")
+                .title("EtudeAI API")
                 .version("1.0")
-                .description("This is a sample API documented with Swagger/OpenAPI"));
+                .description("EtudeAI Application API"))
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .components(new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .description("Enter your JWT token")));
     }
 }
